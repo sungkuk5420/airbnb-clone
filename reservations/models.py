c88d9445d6ba2f532e019b160f7d7e0b68fad59c
sungkuk5420 <sungkuk5420@gmail.com>
2021-04-08 10:42:58 +0900

    예약 어드민 패널 커스텀

--- reservations/models.py
+++ reservations/models.py
@@ -1,7 +1,6 @@
 from django.db import models
+from django.utils import timezone
 from core import models as core_models
-
-# Create your models here.
 
 
 class Reservation(core_models.TimeStampedModel):
@@ -32,3 +31,15 @@ class Reservation(core_models.TimeStampedModel):
 
     def __str__(self):
         return f"{self.room} - {self.check_in}"
+
+    def in_progress(self):
+        now = timezone.now().date()
+        return now > self.check_in and now < self.check_out
+
+    in_progress.boolean = True
+
+    def is_finished(self):
+        now = timezone.now().date()
+        return now > self.check_out
+
+    is_finished.boolean = True
