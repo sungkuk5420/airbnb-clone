30f2ccfb9f0f45e437b2b88ef68039ea742a71fd
sungkuk5420 <sungkuk5420@gmail.com>
2021-04-12 17:40:41 +0900

    룸 상세화면 데이터베이스 연결

--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django_countries.fields import CountryField
 from django.urls import reverse
+from django_countries.fields import CountryField
 from core import models as core_models
 
 
@@ -103,9 +103,8 @@ class Room(core_models.TimeStampedModel):
     def total_rating(self):
         all_reviews = self.reviews.all()
         all_ratings = 0
-        for review in all_reviews:
-            all_ratings += review.rating_average()
         if len(all_reviews) > 0:
-            return all_ratings / len(all_reviews)
-        else:
-            return 0
+            for review in all_reviews:
+                all_ratings += review.rating_average()
+            return round(all_ratings / len(all_reviews), 2)
+        return 0
