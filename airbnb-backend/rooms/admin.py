58a61af6140cefe77e240d0d32f7a5dbe3f5c3ac
sungkuk5420 <sungkuk5420@gmail.com>
2022-12-18 00:40:28 +0900

    새로운 버전으로 에어비앤비 리뉴얼중 7강 까지 완료함.

---
+++ airbnb-backend/rooms/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from .models import Room, Amenity
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "total_amenities",
+        "owner",
+        "created_at",
+    )
+
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "amenities",
+        "created_at",
+        "updated_at",
+    )
+
+    def total_amenities(self,room):
+        print(room)
+        return room.amenities.count()
+
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
