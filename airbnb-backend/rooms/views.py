a1602edfa60ddf30ee014201db7f54528aa3bc80
sungkuk5420 <sungkuk5420@gmail.com>
2022-12-19 20:39:44 +0900

    방에 사진 추가할 수 있도록하기. 모든 인증에 대한 검증은 permission_classes를 통해서 하기.

--- airbnb-backend/rooms/views.py
+++ airbnb-backend/rooms/views.py
@@ -1,27 +1,35 @@
+from django.conf import settings
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError, PermissionDenied
-from rest_framework.status import HTTP_204_NO_CONTENT
 from django.db import transaction
-from categories.models import Category
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
+from rest_framework.exceptions import (
+    NotFound,
+    NotAuthenticated,
+    ParseError,
+    PermissionDenied,
+)
 from .models import Amenity, Room
+from categories.models import Category
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 from reviews.serializers import ReviewSerializer
-from django.core.paginator import Paginator
+from medias.serializers import PhotoSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 class Amenities(APIView):
     def get(self, request):
-        amenities = Amenity.objects.all()
-        serializer = AmenitySerializer(amenities, many=True)
-
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True)
         return Response(serializer.data)
 
     def post(self, request):
         serializer = AmenitySerializer(data=request.data)
-
         if serializer.is_valid():
             amenity = serializer.save()
-            return Response(AmenitySerializer(amenity).data)
+            return Response(
+                AmenitySerializer(amenity).data,
+            )
         else:
             return Response(serializer.errors)
 
@@ -36,7 +44,6 @@ class AmenityDetail(APIView):
     def get(self, request, pk):
         amenity = self.get_object(pk)
         serializer = AmenitySerializer(amenity)
-
         return Response(serializer.data)
 
     def put(self, request, pk):
@@ -48,68 +55,69 @@ class AmenityDetail(APIView):
         )
         if serializer.is_valid():
             updated_amenity = serializer.save()
-            return Response(AmenitySerializer(updated_amenity).data)
+            return Response(
+                AmenitySerializer(updated_amenity).data,
+            )
         else:
             return Response(serializer.errors)
 
     def delete(self, request, pk):
         amenity = self.get_object(pk)
         amenity.delete()
-
         return Response(status=HTTP_204_NO_CONTENT)
 
 
 class Rooms(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get(self, request):
-        rooms = Room.objects.all()
+        all_rooms = Room.objects.all()
         serializer = RoomListSerializer(
             all_rooms,
             many=True,
             context={"request": request},
         )
-
         return Response(serializer.data)
 
     def post(self, request):
-        if request.user.is_authenticated:
-            serializer = RoomDetailSerializer(data=request.data)
-
-            if serializer.is_valid():
-                category_pk = request.data.get("category")
-                if not category_pk:
-                    raise ParseError("Category is required")
-
-                try:
-                    category = Category.objects.get(pk=category_pk)
-                    if category.kind == Category.CategoryKindChoices.EXPERIENCES:
-                        raise ParseError("The category kind should be rooms")
-                except Category.DoesNotExist:
-                    raise ParseError(detail="Category not found")
-
-                try:
-                    with transaction.atomic():
-                        room = serializer.save(owner=request.user, category=category)
-
-                        amenities = request.data.get("amenities")
-                        for amenity_pk in amenities:
-                            amenity = Amenity.objects.get(pk=amenity_pk)
-                            room.amenities.add(amenity)
-
-                        return Response(RoomDetailSerializer(room).data)
-                except Exception:
-                    raise ParseError("amenity not found")
-
-            else:
-                return Response(serializer.errors)
+        serializer = RoomDetailSerializer(data=request.data)
+        if serializer.is_valid():
+            category_pk = request.data.get("category")
+            if not category_pk:
+                raise ParseError("Category is required.")
+            try:
+                category = Category.objects.get(pk=category_pk)
+                if category.kind == Category.CategoryKindChoices.EXPERIENCES:
+                    raise ParseError("The category kind should be 'rooms'")
+            except Category.DoesNotExist:
+                raise ParseError("Category not found")
+            try:
+                with transaction.atomic():
+                    room = serializer.save(
+                        owner=request.user,
+                        category=category,
+                    )
+                    amenities = request.data.get("amenities")
+                    for amenity_pk in amenities:
+                        amenity = Amenity.objects.get(pk=amenity_pk)
+                        room.amenities.add(amenity)
+                    serializer = RoomDetailSerializer(room)
+                    return Response(serializer.data)
+            except Exception:
+                raise ParseError("Amenity not found")
         else:
-            raise NotAuthenticated
+            return Response(serializer.errors)
 
 
 class RoomDetail(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get_object(self, pk):
         try:
             return Room.objects.get(pk=pk)
-        except:
+        except Room.DoesNotExist:
             raise NotFound
 
     def get(self, request, pk):
@@ -118,17 +126,10 @@ class RoomDetail(APIView):
             room,
             context={"request": request},
         )
-
         return Response(serializer.data)
 
     def put(self, request, pk):
         room = self.get_object(pk)
-
-        if not request.user.is_authenticated:
-            raise NotAuthenticated
-
-        if room.owner != request.user:
-            raise PermissionDenied
 
         serializer = RoomDetailSerializer(
             room,
@@ -169,15 +170,9 @@ class RoomDetail(APIView):
 
     def delete(self, request, pk):
         room = self.get_object(pk)
-
-        if not request.user.is_authenticated:
-            raise NotAuthenticated
-
         if room.owner != request.user:
             raise PermissionDenied
-
         room.delete()
-
         return Response(status=HTTP_204_NO_CONTENT)
 
 
@@ -194,10 +189,35 @@ class RoomReviews(APIView):
             page = int(page)
         except ValueError:
             page = 1
-
-        page_size = 3
-
+        page_size = settings.PAGE_SIZE
+        start = (page - 1) * page_size
+        end = start + page_size
         room = self.get_object(pk)
-        paginator = Paginator(room.reviews.all(), page_size, orphans=2)
-        serializer = ReviewSerializer(paginator.get_page(page), many=True)
+        serializer = ReviewSerializer(
+            room.reviews.all()[start:end],
+            many=True,
+        )
         return Response(serializer.data)
+
+
+class RoomPhotos(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        if request.user != room.owner:
+            raise PermissionDenied
+        serializer = PhotoSerializer(data=request.data)
+        if serializer.is_valid():
+            photo = serializer.save(room=room)
+            serializer = PhotoSerializer(photo)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
